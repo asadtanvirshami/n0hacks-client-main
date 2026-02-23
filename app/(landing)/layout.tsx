f5b776f0648fca8c2f70bfba6796ae0871d2d114
Asad Tanvir Shami <91515695+asadtanvirshami@users.noreply.github.com>
2026-02-24 04:46:11 +0500

    push

--- app/(landing)/layout.tsx
+++ app/(landing)/layout.tsx
@@ -2,7 +2,7 @@ import MainLayout from "@/components/app/layout/main-layout";
 import React from "react";
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
-  return <MainLayout children={children}/>
+  return <MainLayout>{children}</MainLayout>;
 };
 
 export default Layout;
--- components/app/layout/main-layout.tsx
+++ components/app/layout/main-layout.tsx
@@ -1,11 +1,7 @@
-import React from 'react'
+import React from "react";
 
 const MainLayout = ({ children }: { children: React.ReactNode }) => {
-  return (
-    <>
-      {children}
-    </>
-  )
-}
+  return <>{children}</>;
+};
 
-export default MainLayout
+export default MainLayout;
